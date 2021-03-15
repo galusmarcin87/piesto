d1437b6e40b014ba79c443a96616ec7234a4b065
mgalus <mgalus@dreamlab.pl>
2021-03-15 08:08:36 +0100

    video https

--- web/js/scriprs.js
+++ web/js/scriprs.js
@@ -343,7 +343,7 @@ $(document).ready(function () {
           youtube: {
             index: 'youtube.com/',
             id: 'v=',
-            src: 'http://www.youtube.com/embed/%id%?autoplay=1',
+            src: 'https://www.youtube.com/embed/%id%?autoplay=1',
           },
         },
         srcAction: 'iframe_src',
